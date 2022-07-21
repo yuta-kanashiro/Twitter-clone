d9851a543d21093ac325de40684ff75c84d9d63a
金城 雄太 <kanashiroyuuta@Hajimari-kanashiroyuta.local>
2022-07-21 10:40:18 +0900

    シーダーファイル（ダミーデータ）作成完了

---
+++ src/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'account_name' => 'sample1',
+                'user_name' => 'sample1sample1',
+                'email' => 'sample1@sample.com',
+                'password' => bcrypt('sample1sample1'),
+                'introduction' => 'みなさんこんにちは。僕の名前はsample1です。この文章は自己紹介のサンプルです。',
+                'profile_image' => null,
+                'remember_token' => Str::random(10),
+            ],
+        ]);
+
+        User::factory()->count(50)->create(); 
+    }
+}
