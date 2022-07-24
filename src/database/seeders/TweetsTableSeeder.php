bccb390d2884380a44fe2e8d714d77af8db874b5
金城 雄太 <kanashiroyuuta@Hajimari-kanashiroyuta.local>
2022-07-24 11:04:40 +0900

    use宣言並べ替え

--- src/database/seeders/TweetsTableSeeder.php
+++ src/database/seeders/TweetsTableSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Tweet;
+use Illuminate\Database\Seeder;
 
 class TweetsTableSeeder extends Seeder
 {
--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
