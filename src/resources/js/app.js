b3a78d1573b1da6787935f8dd0cee99caaf2b8ac
金城 雄太 <kanashiroyuuta@Hajimari-kanashiroyuta.local>
2022-07-29 12:11:48 +0900

    メンターにgithub共有するためのコミット

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('SampleComponent', require('./components/SampleComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -28,5 +28,9 @@
         <div class="mt-5">
             @yield('content')
         </div>
+
+        <SampleComponent></SampleComponent>
+    </div>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 </html>
