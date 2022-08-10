0a63a49b08ff2dfc6a899215a7b42a8e385c38a8
金城 雄太 <kanashiroyuuta@Hajimari-kanashiroyuta.local>
2022-08-10 15:32:03 +0900

    UseCase名の変更、PHPdocの追加（間違ってUseCaseファイル削除したので追加）

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\UseCase\User\ShowUserListUseCase;
+use App\UseCase\User\IndexUseCase;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(ShowUserListUseCase $useCase): object
+    * 全ユーザー情報の取得
+    * 
+    * @param  App\UseCase\User\IndexUseCase $useCase
+    * @return object
+    */
+    public function index(IndexUseCase $useCase): object
     {
         return $useCase->getUserList();
     }
--- src/app/UseCase/User/IndexUseCase.php
+++ src/app/UseCase/User/IndexUseCase.php
@@ -4,9 +4,13 @@ namespace App\UseCase\User;
 
 use App\Models\User;
 
-final class ShowUserListUseCase
+final class IndexUseCase
 {
-
+    /**
+    * 全ユーザー情報の取得
+    * 
+    * @return object
+    */
     public function getUserList(): object
     {
         $users = User::orderBy('created_at', 'desc')->get();
