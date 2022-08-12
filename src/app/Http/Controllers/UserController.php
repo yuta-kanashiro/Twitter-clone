461839a69b96f8e56dfc34f3bfbee9fe14c18293
金城 雄太 <kanashiroyuuta@Hajimari-kanashiroyuta.local>
2022-08-12 11:42:57 +0900

    PHPdoc記述変更

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     * 全ユーザー情報の取得
     * 
     * @param  Index $useCase
-    * @return object
+    * @return Collection
     */
     public function index(Index $useCase): Collection
     {
